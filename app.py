2cb975b09c2d5557ae9b182bab74ffff1334d40c
TheoNorris <theonorris@live.com>
2020-07-08 07:15:38 +0000

    Environment variable added - Database password

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env 
 
 app = Flask(__name__)
+
+app.config["MONGODB_NAME"] = "task_manager"
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def hello():
-    return 'Hello World...again'
+    return 'Hello, World'
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
