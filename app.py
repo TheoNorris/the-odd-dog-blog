9d3b0dfaf4612c8b059be95a5b8741f79968829e
TheoNorris <theonorris@live.com>
2020-07-08 14:46:27 +0000

    Added modal.html, created the correct input sections for the discussions form, added datetime on app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from flask import Flask,render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -17,6 +18,10 @@ mongo = PyMongo(app)
 @app.route('/discussions')
 def discussions():
     return render_template('discussions.html', comments=mongo.db.comments.find())
+
+@app.route('/start_discussion')
+def start_discussion():
+    return render_template('start_discussion.html' , comments=mongo.db.comments.find())
 
 
 if __name__ == '__main__':
