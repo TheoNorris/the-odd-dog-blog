14490c8df8dc4fb5806556fc6a07797cf160ba5b
TheoNorris <theonorris@live.com>
2020-07-09 12:53:24 +0000

    added basic edit form +  added adjacent routes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import itertools
 import os
 from datetime import datetime
-from flask import Flask,render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -22,7 +21,7 @@ def discussions():
 
 @app.route('/start_discussion')
 def start_discussion():
-    return render_template('start_discussion.html', now = datetime.now().strftime("%D, %H:%M"), comments=mongo.db.comments.find())
+    return render_template('start_discussion.html', now = datetime.now().strftime("%D, %H:%M"), categories=mongo.db.categories.find())
 
 @app.route('/insert_discussion', methods=['POST'])
 def insert_discussion():
@@ -30,9 +29,24 @@ def insert_discussion():
     comments.insert_one(request.FILES.form.to_dict())  
     return redirect(url_for('discussions'))
 
-@app.route('/edit_discussion/<comments_id>')
-def edit_discussion(comments_id):
-    return render_template('edit_discussion.html', now = datetime.now().strftime("%D, %H:%M"), comments=mongo.db.comments.find())
+@app.route('/edit_discussion/<comment_id>')
+def edit_discussion(comment_id):
+    the_comment = mongo.db.comments.find_one({"_id": ObjectId(comment_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edit_discussion.html', now = datetime.now().strftime("%D, %H:%M"), comment=the_comment, categories=all_categories)
+
+@app.route('/update_discussion/<comment_id>', methods=["POST"])
+def update_discussion(comment_id):
+    comments = mongo.db.comments
+    comments.update({'_id': ObjectId(comment_id)}, 
+    {
+        'category_name': request.form.get('category_name'),
+        'date_time': request.form.get('date_time'),
+        'title': request.form.get('title'),
+        'comment': request.form.get('comment'),
+    })
+    return redirect(url_for('discussions'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
